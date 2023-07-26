e567932b259ca79bb3fa875aee16af9e28d3e9c0
Mansi joshi <joshimansi32@gmail.com>
2023-07-26 16:34:50 +0530

    Update solution.cpp

--- MedianMatrix/solution.cpp
+++ MedianMatrix/solution.cpp
@@ -1,30 +1,36 @@
-int CountSmallerThanEqualToMid(vector<int>& row, int mid){
-    int l =0;
-    int h =row.size()-1;
-
+int CountNumber(vector<int> row,int mid){
+    int l=0, h=row.size()-1;
     while(l<=h){
-        int md = (l+h)>>1;
-        if(row[md]>mid) l = md+1;
-        else h = md-1;
+        int md=(l+h)>>1;
+        if(row[md]<=mid){
+            l=md+1;
+        }
+        else{
+            h=md-1;
+        }
     }
-    return 1;
+    return l;
 }
 
-int getMedian(vector<vector<int>>& matrix){
-    int low = 1;
-    int high = 1e5;
+ 
 
-    int n = matrix.size();
-    int m = matrix[0].size();
+int getMedian(vector<vector<int>> &matrix)
+{
+    int low=1;
+    int high=1e9;
+    int n=matrix.size();
+    int m=matrix[0].size();
 
     while(low<=high){
-        int mid = (low+high)>>1;
-        int count = 0; // count the total elements that are <=mid
-        for(int i=0;i<n;i++){
-            counnt+=CountSmallerThanEqualToMid(matrix[i],mid);
-        }
-        if(count>(n*m)/2) low = mid+1;
-        else high = mid-1;
+        int mid=(low+high)>>1;
+        int cnt=0;
+         for(int i=0;i<n;i++){
+             cnt+=CountNumber(matrix[i],mid);
+         }
+         if (cnt<=(n*m)/2) low=mid+1;
+         else   high=mid-1;
     }
+
     return low;
+
 }
